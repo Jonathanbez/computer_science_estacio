41a13ce357268230d058407cc5339bf538b05bc0
Jonathan <user.name>
2025-03-11 00:55:03 -0300

    Estrutura de decisão composta - desafio hora de codar

---
+++ t2_nn2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    float   temperatura,    umidade;
+    unsigned int    estoque, estoqueMinimo;
+
+    printf("Digite a temperetura: \n");
+    scanf("%f", &temperatura);
+    printf("Digite a temperatura: \n");
+    scanf("%f", &umidade);
+    print("Digite o estoque: \n");
+    scanf("%u", &estoque);
+
+    if(temperatura > 30){
+        printf("A temperatura esta alta.\n"); 
+    }else{
+        printf("Temperatura dentro dos parametros.\n");
+    }
+
+    if(umidade > 50){
+        printf("Umidade elevada.\n");
+    }else{
+        printf("Umidade dentro dos parametros.\n");
+    }
+    if(estoque < estoqueMinimo){
+        printf("Estoque abaixo do limite.\n");
+    }else{
+        printf("Estoque dentro dos parametros.\n")
+    }
+}
