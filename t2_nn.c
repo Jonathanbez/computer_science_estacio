18109544126d21f9f5a9139cdcc85aed95c39ac8
Jonathan <user.name>
2025-03-10 22:25:08 -0300

    Estrutura de decisão composta

---
+++ t2_nn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int maiorIdade = 18;
+    int atualIdade;
+
+    printf("Digite sua idade: \n");
+    scanf("d%", &atualIdade);
+    
+    if(atualIdade >= maiorIdade){
+        printf("Voce é maior de idade!");
+    }
+    else{
+        printf("Voce ainda é de menor!");
+    }
+}
