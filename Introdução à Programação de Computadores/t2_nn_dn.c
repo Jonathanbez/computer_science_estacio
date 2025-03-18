ee91676cf691976862c08de13d653df46f864eaa
Jonathanbez <jbbarroz@gmail.com>
2025-03-18 00:01:41 -0300

    Desafio: nível novato - adjustmenst and tests v0.6

--- Introdução à Programação de Computadores/t2_nn_dn.c
+++ Introdução à Programação de Computadores/t2_nn_dn.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    // Variaveis
+    // VARIAVEIS
     char ufEstado[4];
     char codigoCarta[5];
     char nomeCidade[25];
@@ -22,13 +22,12 @@ int main() {
 
     printf("SUPER TRUNFO!\n");
 
-    // 1° CARTA
+    // CADASTRAMENTO 1° CARTA
     printf("CADASTRE A 1° CARTA\n");    
     printf("Digite o UF do Estado: \n");
     fgets(ufEstado, 4, stdin);
-      
     printf("Digite codigo da carta: \n");
-    scanf(codigoCarta, 5, stdin);
+    fgets(codigoCarta, 5, stdin);
 
     printf("Digite nome da cidade: \n");
     fgets(nomeCidade, 25, stdin);
@@ -46,7 +45,7 @@ int main() {
     scanf("%d", &pontos_turistico);
     getchar();
 
-    // 2° CARTA
+    // CADASTRAMENTO 2° CARTA
     printf("\nSPCADASTRE A 2° CARTA\n");
     
     printf("Digite o UF do Estado: \n");
@@ -67,15 +66,15 @@ int main() {
     printf("Digite o PIB: \n");
     scanf("%f", &PIB2);
 
-    printf("Digite Pontos Turisticos: \n\n");
+    printf("Digite Pontos Turisticos: \n");
     scanf("%d", &pontos_turistico2);
 
-    //Exibe dados cadasstrados (1° carta)
-    printf("1° Carta: %s\n", codigoCarta);
-    printf("1° Estado  %s\n", ufEstado);
+    //EXIBE DADOS CARTA 2 (1° carta)
+    printf("DADOS CARTA 1\n");
+    printf("1° Codigo Carta: %s", codigoCarta);
+    printf("1° Estado  %s", ufEstado);
     printf("1° Cidade: %s", nomeCidade);
-    printf("1° Codigo: %s\n", codigoCarta);
-    printf("1° População: %i\n", populacao);
+    printf("1° População: %i", populacao);
     printf("1° Área: %.2f\n", area_cidade);
     printf("1° PIB: %.2f\n", PIB);
     printf("1° Pontos Turisticos: %d\n", pontos_turistico);
@@ -84,18 +83,17 @@ int main() {
     printf("1° Densidade Populacional: %f\n", densidade);
     printf("2° PIB per capita: %f\n\n", PIBper);
 
-    //Exibe dados cadasstrados (2° carta)
-    printf("2° Carta: %s\n", codigoCarta2);
-    printf("2° Estado  %s\n", ufEstado2);
-    printf("2° Cidade: %s", nomeCidade2);
-    printf("2° Codigo: %s\n", codigoCarta2);
+    //EXIBE DADOS CARTA 2 (2° carta)
+    printf("DADOS CARTA 1\n");
+    printf("2° Codigo Carta: %s", codigoCarta2);
+    printf("2° Estado  %s", ufEstado2);
+    printf("2° Cidade: %s", nomeCidade2);;
     printf("2° População: %i\n", populacao2);
     printf("2° Área: %.2f\n", area_cidade2);
     printf("2° PIB: %.2f\n", PIB);
     printf("2° Pontos Turisticos: %d\n", pontos_turistico2);
     float densidade2 = populacao2 / area_cidade2;
     float PIBper2 = PIB / populacao2;
-
     printf("1° Densidade Populacional: %f\n", densidade2);
     printf("2° PIB per capita: %f\n\n", PIBper2);
 
