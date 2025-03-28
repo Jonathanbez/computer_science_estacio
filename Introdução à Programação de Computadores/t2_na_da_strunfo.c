c1dd7d0ae62530877d7d9d098d891c94e06f4046
Jonathanbez <jbbarroz@gmail.com>
2025-03-28 08:38:32 -0300

    Tema 2 - Desafio  Nivel Aventureiro v0.1

---
+++ Introdução à Programação de Computadores/t2_na_da_strunfo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <<time.h>
+
+int main(){
+    int populcao, pontosTuristicos;
+    char nomePais[60];
+    float area, pib, densdemografica;
+
+    
+}
